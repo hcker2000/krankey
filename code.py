3bb5114cd59acddd589c1ac7a680723e0ca43eda
Mike Ashcraft <mashcraft@mach2.org>
2024-01-17 14:30:36 -0500

    done for now

--- code.py
+++ code.py
@@ -23,13 +23,17 @@ from DFPlayer import DFPlayer
 
 	
 # --- constants   ----------------------------------------------------------
-BG_PLAYER_VOL = 70
+BG_PLAYER_VOL = 40
 BG_PLAYER_TX = board.GP16
 BG_PLAYER_RX = board.GP17
 
 VOICE_PLAYER_VOL = 100
 VOICE_PLAYER_TX = board.GP16
 VOICE_PLAYER_RX = board.GP17
+
+#5 to 15 minute random delay
+VOICE_MIN_DELAY = 300
+VOICE_MAX_DELAY = 900
 
 # --- objects   -----------------------------------------------------------
 bgUart = busio.UART(board.GP16, board.GP17, baudrate=9600)
@@ -46,12 +50,12 @@ firstLoop = True
 introPlaying = True
 voicePlaying = False
 switchTrack = False
-nextVoiceTime = random.randint(1,2)
+nextVoiceTime = random.randint(VOICE_MIN_DELAY,VOICE_MAX_DELAY)
 lastVoiceTime = time.monotonic()
+lastLoopTime = time.monotonic_ns()
 
-# get random number between 1 and 100 
-bgRandom = random.randint(1, 2)
-
+# get random number between 1 and 10 
+bgRandom = random.randint(1, 10)
 
 voicePlayer.play(track=1)
  
@@ -63,45 +67,40 @@ def generateNextVoiceTime():
 	global lastVoiceTime
  
 	voicePlaying = False
-	nextVoiceTime = random.randint(1,8)
+	nextVoiceTime = random.randint(VOICE_MIN_DELAY,VOICE_MAX_DELAY)
 	lastVoiceTime = time.monotonic()
 
 # --- main loop   --------------------------------------------------------
 
 while True:
-	time.sleep(0.100)
- 
-	if voicePlayer.get_status() != 1:
-		introPlaying = False
+	if time.monotonic_ns() - lastLoopTime > 100:
+		lastLoopTime = time.monotonic_ns()
+		if voicePlayer.get_status() != 1:
+			introPlaying = False
 
-	if introPlaying == False:
-		if firstLoop == True:
-			firstLoop = False
-			if bgRandom == 1:
-				bgPlayer.play(track=1)
+		if introPlaying == False:
+			if firstLoop == True:
+				firstLoop = False
+				if bgRandom > 1:
+					bgPlayer.play(track=1)
+					bgPlayer.loop()
+				else:
+					switchTrack = True
+					bgPlayer.play(track=2)
+		
+			if bgPlayer.get_status() == 1:
+				led.value = True
 			else:
-				switchTrack = True
-				bgPlayer.play(track=2)
-    
-		if bgPlayer.get_status() == 1:
-			led.value = True
-			# print("busy")
-		else:
-			if switchTrack == True:
-				switchTrack = False
-				bgPlayer.play(track=1)
-			led.value = False
-		print(voicePlaying)
-		print(nextVoiceTime)
-		print(time.monotonic() - lastVoiceTime)
-		if voicePlaying == False:
-			if nextVoiceTime < time.monotonic() - lastVoiceTime:
-				voicePlaying = True
-				bgPlayer.set_volume(25)
-				voicePlayer.play(track=random.randint(2,21))
-		else:
-			if voicePlayer.get_status() != 1:
-				bgPlayer.set_volume(BG_PLAYER_VOL)
-				generateNextVoiceTime()
-
-    
+				if switchTrack == True:
+					switchTrack = False
+					bgPlayer.play(track=1)
+				led.value = False
+			if voicePlaying == False:
+				if nextVoiceTime < time.monotonic() - lastVoiceTime:
+					voicePlaying = True
+					bgPlayer.set_volume(25)
+					voicePlayer.play(track=random.randint(2,21))
+			else:
+				if voicePlayer.get_status() != 1:
+					bgPlayer.set_volume(BG_PLAYER_VOL)
+					generateNextVoiceTime()
